9cd7126a67aa5f560d279d47cd850c02492df51b
w0lfl0w <pazwolfwolfpaz@gmail.com>
2023-01-11 08:56:47 +0200

    started project

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
 //
 // Created by wolflow on 1/11/23.
 //
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <list>
+#include <cmath>
+
+using std::FILE;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::ifstream;
+using std::stringstream;
+
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+}
+
